cce9aba7975e6e1bbe0cc9322c4033593ab04cc6
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-04-17 23:29:34 -0400

    dice added

--- src/main/java/com/term_project/game/Dice.java
+++ src/main/java/com/term_project/game/Dice.java
@@ -1,6 +1,7 @@
 package com.term_project.game;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This class represents dice used in the game betrayal.
@@ -8,6 +9,7 @@ import java.util.ArrayList;
  * @author Zachary Hoffman
  */
 public final class Dice {
+  public static final int MAX_DIE = 2;
   /** Classes should have private constructors rather than no constructor.
    *
    */
@@ -18,9 +20,12 @@ public final class Dice {
    * @return An integer representing a x position.
    *
    */
-  public List<int> roll(int numRoll) {
-    List<int> rolls = new ArrayList<>();
-    for (int i = 0; i < numRoll; i++) {
-      
+  public List<Integer> roll(int numRolls) {
+    List<Integer> rolls = new ArrayList<>();
+    for (int i = 0; i < numRolls; i++) {
+      Integer randInt = ThreadLocalRandom.current().nextInt(0, MAX_DIE + 1);
+      rolls.add(randInt);
     }
+    return rolls;
+  }
 }
