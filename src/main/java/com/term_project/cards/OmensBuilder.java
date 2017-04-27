ba5dd1e499eb6aa0ecf7d668f3f63f6d36755d93
Jack Facey <jfacey@Jacks-MBP-2.fios-router.home>
2017-04-27 02:39:04 -0400

    builders

--- src/main/java/com/term_project/cards/OmensBuilder.java
+++ src/main/java/com/term_project/cards/OmensBuilder.java
@@ -10,18 +10,14 @@ import java.util.stream.IntStream;
 
 public class OmensBuilder implements Builder<Omen> {
 
-private static int DECK_LENGTH = 10;
-	
 	@Override
 	public Queue<Omen> buildDeck() {
-		Queue<Omen> omenDeck = new LinkedList<>();
-		List<Integer> range = IntStream.rangeClosed(0, DECK_LENGTH)
-			    .boxed().collect(Collectors.toList());
-		Collections.shuffle(range);
+		List<Omen> omenList = new ArrayList<>();
 		
-		for (int i = 0; i < DECK_LENGTH; i++) {
-			// TODO: build each omen and add to deck
-		}
+		omenList.add(new ExampleOmen());
+		
+		Collections.shuffle(omenList);
+		Queue<Omen> omenDeck = new LinkedList<>(omenList);
 		return omenDeck;
 	}
 }
