e9e45c605147a812840a39fb4d3785dffd958124
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-04-27 01:59:53 -0400

    cleanslate prehaunt

--- src/main/java/com/term_project/game/haunts/GamePhase.java
+++ src/main/java/com/term_project/game/haunts/GamePhase.java
@@ -4,32 +4,14 @@ import com.term_project.character.GameChar;
 import com.term_project.game.actions.Action;
 import java.util.List;
 import java.util.Set;
+import spark.QueryParamsMap;
 
 public interface GamePhase {
-	String mode;
-	int phase;
 	/**
 	 * Executes an action by the specified character.
 	 * @param character The character doing the specified action.
 	 * @param action The action to be executed.
 	 * @param specs Special specifications for said action.
 	 */
-	public void action(GameChar character, String action, String specs)
-			throws RuntimeException;
-
-	public boolean win();
-
-	public Set<Action> getActions();
-
-	/**
-	 * Starts character's turn.
-	 * @param character Character turn will start.
-	 */
-	void startTurn(GameChar character);
-
-	/**
-	 * Ends character's turn.
-	 * @param character Character whose turn it is now.
-	 */
-	void endTurn(GameChar character);
+	public void run(String name, QueryParamsMap qm);
 }
--- src/main/java/com/term_project/game/haunts/PreHaunt.java
+++ src/main/java/com/term_project/game/haunts/PreHaunt.java
@@ -14,37 +14,10 @@ import java.util.HashSet;
 
 public class PreHaunt implements GamePhase {
 	private MemorySlot gameMemory;
-	private Map<String, Action> actions;
-	private Map<String, Integer> remaining;
+
 	public PreHaunt(MemorySlot gameMemory) {
 		this.gameMemory = gameMemory;
 	}
 
-	@Override
-	public void action(GameChar character, String action, String specs)
-			throws RuntimeException {
-
-		Action toExecute = actions.get(action);
-		toExecute.execute(gameMemory, character, specs, remaining);
-	}
-
-	@Override
-	public boolean win() {
-		return false;
-	}
-
-	@Override
-	public Set<Action> getActions() {
-		return new HashSet<>(actions.values());
-	}
-
-	@Override
-	public void startTurn(GameChar character) {
-		remaining.put("Move", character.getSpeed());
-	}
-
-	@Override
-	public void endTurn(GameChar character) {
-
-	}
+	
 }
