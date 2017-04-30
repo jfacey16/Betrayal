3f8cf527d0727305dcebb677af8d73c6fc760029
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-04-30 04:35:37 -0400

    styling:

--- src/main/java/com/term_project/game/haunts/Lobby.java
+++ src/main/java/com/term_project/game/haunts/Lobby.java
@@ -62,85 +62,85 @@ public class Lobby implements GamePhase {
                                        frontDoorAvailable);
       frontDoor.setPos(new Pos(0, 0, Floor.GROUND));
 
-     //between door and stairs
-     Map<Direction, Tile> midConnected = new HashMap<>();
-     List<Floor> midAvailable = new ArrayList<>();
-     midAvailable.add(Floor.GROUND);
-     Tile mid = new GenericTile(midConnected, 0, 0, 0, midAvailable);
-     mid.setPos(new Pos(-1, 0, Floor.GROUND));
+      //between door and stairs
+      Map<Direction, Tile> midConnected = new HashMap<>();
+      List<Floor> midAvailable = new ArrayList<>();
+      midAvailable.add(Floor.GROUND);
+      Tile mid = new GenericTile(midConnected, 0, 0, 0, midAvailable);
+      mid.setPos(new Pos(-1, 0, Floor.GROUND));
 
-     //stairs
-     Map<Direction, Tile> stairsConnected = new HashMap<>();
-     List<Floor> stairsAvailable = new ArrayList<>();
-     stairsAvailable.add(Floor.GROUND);
-     Tile stairs = new GenericTile(stairsConnected, 0, 0, 0, stairsAvailable);
-     stairs.setPos(new Pos(-2, 0, Floor.GROUND));
+      //stairs
+      Map<Direction, Tile> stairsConnected = new HashMap<>();
+      List<Floor> stairsAvailable = new ArrayList<>();
+      stairsAvailable.add(Floor.GROUND);
+      Tile stairs = new GenericTile(stairsConnected, 0, 0, 0, stairsAvailable);
+      stairs.setPos(new Pos(-2, 0, Floor.GROUND));
 
-     //upper landing
-     Map<Direction, Tile> upperConnected = new HashMap<>();
-     List<Floor> upperAvailable = new ArrayList<>();
-     upperAvailable.add(Floor.ATTIC);
-     Tile upper = new GenericTile(upperConnected, 0, 0, 0, upperAvailable);
-     upper.setPos(new Pos(-2, 0, Floor.ATTIC));
+      //upper landing
+      Map<Direction, Tile> upperConnected = new HashMap<>();
+      List<Floor> upperAvailable = new ArrayList<>();
+      upperAvailable.add(Floor.ATTIC);
+      Tile upper = new GenericTile(upperConnected, 0, 0, 0, upperAvailable);
+      upper.setPos(new Pos(-2, 0, Floor.ATTIC));
 
-     //basement
-     Map<Direction, Tile> basementConnected = new HashMap<>();
-     List<Floor> basementAvailable = new ArrayList<>();
-     basementAvailable.add(Floor.BASEMENT);
-     Tile basement = new GenericTile(basementConnected, 0, 0, 0, basementAvailable);
-     basement.setPos(new Pos(0, 0, Floor.BASEMENT));
+      //basement
+      Map<Direction, Tile> basementConnected = new HashMap<>();
+      List<Floor> basementAvailable = new ArrayList<>();
+      basementAvailable.add(Floor.BASEMENT);
+      Tile basement = new GenericTile(basementConnected, 0, 0, 0, basementAvailable);
+      basement.setPos(new Pos(0, 0, Floor.BASEMENT));
 
-     //connect front door
-     frontDoorConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
-     frontDoorConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
-     frontDoorConnected.put(Direction.WEST, mid);
+      //connect front door
+      frontDoorConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
+      frontDoorConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
+      frontDoorConnected.put(Direction.WEST, mid);
 
-     //connect mid door
-     midConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
-     midConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
-     midConnected.put(Direction.EAST, frontDoor);
+      //connect mid door
+      midConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
+      midConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
+      midConnected.put(Direction.EAST, frontDoor);
 
-     //connect stairs
-     stairsConnected.put(Direction.EAST, mid);
-     stairsConnected.put(Direction.UP, upper);
+      //connect stairs
+      stairsConnected.put(Direction.EAST, mid);
+      stairsConnected.put(Direction.UP, upper);
 
-     //connect upper
-     upperConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
-     upperConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
-     upperConnected.put(Direction.EAST, new EmptyTile(new HashMap<>()));
-     upperConnected.put(Direction.WEST, new EmptyTile(new HashMap<>()));
-     upperConnected.put(Direction.DOWN, stairs);
+      //connect upper
+      upperConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
+      upperConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
+      upperConnected.put(Direction.EAST, new EmptyTile(new HashMap<>()));
+      upperConnected.put(Direction.WEST, new EmptyTile(new HashMap<>()));
+      upperConnected.put(Direction.DOWN, stairs);
 
-     //connect basment
-     basementConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
-     basementConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
-     basementConnected.put(Direction.EAST, new EmptyTile(new HashMap<>()));
-     basementConnected.put(Direction.WEST, new EmptyTile(new HashMap<>()));
-
-
-     //add tiles to tileMap
-     Map<Pos, Tile> tileMap = memory.getTileMap();
-     tileMap.put(frontDoor.getPos(), frontDoor);
-     tileMap.put(mid.getPos(), mid);
-     tileMap.put(stairs.getPos(), stairs);
-     tileMap.put(upper.getPos(), upper);
-     tileMap.put(basement.getPos(), basement);
+      //connect basment
+      basementConnected.put(Direction.NORTH, new EmptyTile(new HashMap<>()));
+      basementConnected.put(Direction.SOUTH, new EmptyTile(new HashMap<>()));
+      basementConnected.put(Direction.EAST, new EmptyTile(new HashMap<>()));
+      basementConnected.put(Direction.WEST, new EmptyTile(new HashMap<>()));
 
 
-     //set list of playersCharacters
-     Map<String, GameChar> playersCharacters = new HashMap<>();
-     //gets list of id from memory
-     List<String> ids = memory.getStringList();
+      //add tiles to tileMap
+      Map<Pos, Tile> tileMap = memory.getTileMap();
+      tileMap.put(frontDoor.getPos(), frontDoor);
+      tileMap.put(mid.getPos(), mid);
+      tileMap.put(stairs.getPos(), stairs);
+      tileMap.put(upper.getPos(), upper);
+      tileMap.put(basement.getPos(), basement);
 
-     //loops through and sets characters
-     for (int i = 0; i < ids.size(); i++) {
-       String id = ids.get(i);
-       // GameChar character = CharacterGen.getCharactersByName(qm.value(id));
-       //playersCharacters.put(id, character);
-       //character.setTile(frontDoor);
-     }
-     //set turn order
-     memory.getGameState().setPlayersCharacters(playersCharacters);
+
+      //set list of playersCharacters
+      Map<String, GameChar> playersCharacters = new HashMap<>();
+      //gets list of id from memory
+      List<String> ids = memory.getStringList();
+
+      //loops through and sets characters
+      for (int i = 0; i < ids.size(); i++) {
+        String id = ids.get(i);
+        // GameChar character = CharacterGen.getCharactersByName(qm.value(id));
+        //playersCharacters.put(id, character);
+        //character.setTile(frontDoor);
+      }
+       //set turn order
+       memory.getGameState().setPlayersCharacters(playersCharacters);
 
       //switch game phases
       memory.getGameState().setPhase(new PreHaunt(memory));
