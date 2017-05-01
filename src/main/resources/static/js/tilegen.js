1308a1f33d92acf0766fe8d986a5b27a1f695277
Takayoshi Miyamoto <tmiyamot@cs.brown.edu>
2017-05-01 13:02:34 -0400

    temp

--- src/main/resources/static/js/tilegen.js
+++ src/main/resources/static/js/tilegen.js
@@ -12,6 +12,7 @@ $(document).ready(() => {
       characters.push(characterMapping);
     }
     console.log("HI");
+    /*
     $.post("/tileStart", {"0": "Ox Bellows", "1": "Madame Zostra", "2": "Brandon Jaspers", "3": "Missy Dubourde",
       "4": "Professor Longfellow", "5": "Jenny LeClerc"}, rj2 => {
       const ro2 = JSON.parse(rj2);
@@ -19,5 +20,6 @@ $(document).ready(() => {
       console.log(ro2.tile);
       console.log(ro2.character);
     });
+    */
   });
 });
