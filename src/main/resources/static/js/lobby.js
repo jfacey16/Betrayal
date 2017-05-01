f8beae733ec0d9681fe72279a7fe957901df1d6f
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-05-01 03:21:21 -0400

    fixed errors

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -1,23 +1,12 @@
 $(document).ready(() => {
 
 	const postParameters = {players:"6"};
-<<<<<<< HEAD
-
-
-    console.log("hi");
-=======
->>>>>>> a1f72e205d79fa733d839dd80b0be5037841e047
 
     $.post("/create_game", postParameters, responseJSON => {
 
         // TODO: Parse the JSON response into a JavaScript object. DONE
         const responseObject = JSON.parse(responseJSON);
-<<<<<<< HEAD
 
 				let choices = responseObject.characterChoices;
-				console.log(responseObject.characterChoices);
-
-=======
->>>>>>> a1f72e205d79fa733d839dd80b0be5037841e047
     });
 });
