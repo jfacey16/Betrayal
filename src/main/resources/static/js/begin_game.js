70cab0f9ecaa60cb8f57af896408f101ee5d98c7
Alainey Hellman <ahellma1@cs.brown.edu>
2017-05-08 17:17:56 -0400

    initial setup working

--- src/main/resources/static/js/begin_game.js
+++ src/main/resources/static/js/begin_game.js
@@ -190,7 +190,7 @@ function set_starting_state(data) {
 
 	current_char--;
 	current_turn--;
-	turn_end(current_turn);
+	update_turn(current_turn);
 	const $objective_description = $("#objective_description");
 	$objective_description.html("<center>Something weird is going on... <p>Explore the house and find out what!</p></center>");
 }
